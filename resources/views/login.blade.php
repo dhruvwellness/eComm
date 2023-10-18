de83a6c1bc11c4fc7a9ec853e7a4d7187d2e5017
Dhruvaggr <11.dhruvgupta@gmail.com>
2023-10-19 01:10:59 +0530

    Email changes

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,7 +9,7 @@
         <div class="col-sm-4 col-sm-offset-4 custom-form">
             <form>
                 <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
+                    <label for="exampleInputEmail1">Email Address</label>
                     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
                     <!-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> -->
                 </div>
