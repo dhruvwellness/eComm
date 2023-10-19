1e7d75add09188bcc3200998a82a3660fd035173
Dhruvaggr <11.dhruvgupta@gmail.com>
2023-10-19 09:31:39 +0530

    Laravel Notes are included to study

---
+++ Laravel Notes/git.php
@@ -0,0 +1,14 @@
+
+1. Commit and push 
+a. Using Command line.
+   git add .
+   git remote add origin https://github.com/Dhruvaggr/eComm.git    [Written on git when we create new repository without readme file]
+   git push -u origin main
+
+b. Using Source Control [Extension]
+    commit button or push 
+    Write the name of commit 
+    ctr+s ctrl+w
+    Hurray!.
+    We can also use git push -u origin main after commiting from source control it will commited.
+
